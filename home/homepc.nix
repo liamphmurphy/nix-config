c6a3f9a12c0cbd917554044cf66f81a2c0908f50
Liam Murphy <liam@phmurphy.com>
2025-10-05 15:07:46 -0700

    fixes

--- home/homepc.nix
+++ home/homepc.nix
@@ -37,12 +37,6 @@
     nerd-fonts.iosevka
     nerd-fonts.fira-code
     font-awesome
-
-    # K8s things
-    kubectl
-    kind
-    kubernetes-helm
-    kubectx
   ];
 
 
@@ -61,26 +55,5 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/liam/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "vim";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
